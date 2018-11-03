36d23a7206c216f45bb9f3c23f85a2923cfe4199
pablexagro <43689115+pablexagro@users.noreply.github.com>
2018-11-02 21:13:28 -0600

    Update gpsd_cliente.py

--- gpsd_cliente.py
+++ gpsd_cliente.py
@@ -29,30 +29,32 @@ print ('Socket Creado')
 s.connect((remote_ip , port))
 print ('Socket Conectado a ip: ' + remote_ip + '  puerto: ' + str(port))
 
-while 1:
-    data = s.recv(1)                        # Leer un dato por recorrido
-    _buffer = str(data)                     # Almacena dato en variable auxiliar
-    if _buffer[2] != '\\':                  # Si dato direfente a sentencia de separacion lo agrega a la cadena a comando recibido
-        line_data += _buffer[2]         
-    else:                                   # Si dato de separacion recibo
-        if line_data.find('$GNGGA') != -1:  # Busca sentencia coordenadas nmea
-            #print(line_data)
-            msg = pynmea2.parse(line_data)  # Extancion de informacion de sentencia nmea
-            os.system('cls')
-            total = msg.altitude + float(msg.geo_sep)
-        
-            print('GPSD cliente')
-            print ('Socket Conectado a ip: ' + remote_ip + '  puerto: ' + str(port) + '\n')
-            print('Lat:       ' + str(msg.latitude))
-            print('Lon:       ' + str(msg.longitude))
-            print('Ele:       ' + str(total))
-            print('Sat n:     ' + str(msg.num_sats))
-            print('Gps Qual:   ' + str(msg.gps_qual))
-            print('')
+try:
+    while 1:
+        data = s.recv(1)                        # Leer un dato por recorrido
+        _buffer = str(data)                     # Almacena dato en variable auxiliar
+        if _buffer[2] != '\\':                  # Si dato direfente a sentencia de separacion lo agrega a la cadena a comando recibido
+            line_data += _buffer[2]         
+        else:                                   # Si dato de separacion recibo
+            if line_data.find('$GNGGA') != -1:  # Busca sentencia coordenadas nmea
+                #print(line_data)
+                msg = pynmea2.parse(line_data)  # Extancion de informacion de sentencia nmea
+                os.system('cls')
+                total = msg.altitude + float(msg.geo_sep)
+            
+                print('GPSD cliente')
+                print ('Socket Conectado a ip: ' + remote_ip + '  puerto: ' + str(port) + '\n')
+                print('Lat:       ' + str(msg.latitude))
+                print('Lon:       ' + str(msg.longitude))
+                print('Ele:       ' + str(total))
+                print('Sat n:     ' + str(msg.num_sats))
+                print('Gps Qual:   ' + str(msg.gps_qual))
+                print('')
 
-        line_data = ''                      # Limpiamos linea de sentencia procesada
+            line_data = ''                      # Limpiamos linea de sentencia procesada
+except(KeyboardInterrupt, SystemExit):
+    s.close()
     
-s.close()
 print('\nTerminado por Usuario.\n')
 
 time.sleep(1)
